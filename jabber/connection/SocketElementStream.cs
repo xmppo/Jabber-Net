508252109be051cbfd1152ecee473bfc00bdff5e
hildjj <hildjj@b87ccc53-a82b-0410-9262-3dc31cef668b>
2004-09-14 08:01:03 +0000

    Throw exception if CLR 1.1 SP has been installed.  Hope for brainstorm tomorrow.
    git-svn-id: http://jabber-net.googlecode.com/svn/trunk@236 b87ccc53-a82b-0410-9262-3dc31cef668b

--- jabber/connection/SocketElementStream.cs
+++ jabber/connection/SocketElementStream.cs
@@ -703,6 +703,11 @@ namespace jabber.connection
         /// </summary>
         public virtual void Connect()
         {
+            if (System.Reflection.Assembly.GetExecutingAssembly().ImageRuntimeVersion == "v1.1.4322")
+            {
+                throw new NotSupportedException("This version of the Common Language Runtime is not supported.  You have installed the CLR 1.1 Service Pack 1.  There is no known work-around at this time.");
+            }
+
             Debug.Assert(m_port > 0);
             Debug.Assert(m_to != null);
 			m_conEvent.Reset();
@@ -780,7 +785,14 @@ namespace jabber.connection
         {
             Debug.Assert(m_synch, "Cannot call SynchConnectThread unless in synch mode");
             Address addr = new Address(GetHost(), m_port);
-            m_sock.Connect(addr);
+            try
+            {
+                m_sock.Connect(addr);
+            }
+            catch (Exception e)
+            {
+                FireOnError(e);
+            }
         }
 
         /// <summary>
