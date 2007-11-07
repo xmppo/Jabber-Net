764db601fa6aa473fe61267b4f5a93d9a8bf57e9
hildjj <hildjj@b87ccc53-a82b-0410-9262-3dc31cef668b>
2007-11-07 20:08:09 +0000

    Turn IdleTime into a component, for easier drag/drop.
    git-svn-id: http://jabber-net.googlecode.com/svn/trunk@447 b87ccc53-a82b-0410-9262-3dc31cef668b

--- bedrock/util/IdleTime.cs
+++ bedrock/util/IdleTime.cs
@@ -12,8 +12,10 @@
  * See LICENSE.txt for details.
  * --------------------------------------------------------------------------*/
 using System;
+using System.ComponentModel;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
 
 namespace bedrock.util
 {
@@ -27,7 +29,8 @@ namespace bedrock.util
     /// <summary>
     /// Idle time calculations and notifications.
     /// </summary>
-    public class IdleTime
+    [SVN(@"$Id$")]
+    public class IdleTime : System.ComponentModel.Component
     {
         [StructLayout(LayoutKind.Sequential)]
         private struct LASTINPUTINFO
@@ -62,31 +65,42 @@ namespace bedrock.util
         /// </summary>
         public event SpanEventHandler OnUnIdle;
 
+        private const double DEFAULT_POLL = 5;      // 5s in s.
+        private const double DEFAULT_IDLE = 5 * 60; // 5m in s.
+
         private System.Timers.Timer m_timer = null;
-        private int m_notifySecs;
-        private int m_pollSecs;
+        private double m_notifySecs = DEFAULT_IDLE;
         private bool m_idle = false;
         private DateTime m_idleStart = DateTime.MinValue;
-        private System.ComponentModel.ISynchronizeInvoke m_invoker = null;
+        private ISynchronizeInvoke m_invoker = null;
+
+        /// <summary>
+        /// Create an idle timer with the default timouts.
+        /// </summary>
+        public IdleTime()
+        {
+            m_timer = new System.Timers.Timer(DEFAULT_POLL * 1000.0);
+            m_timer.Elapsed += new System.Timers.ElapsedEventHandler(m_timer_Elapsed);
+        }
 
         /// <summary>
         /// Create an idle timer.  Make sure to set Enabled = true to start.
         /// </summary>
         /// <param name="pollSecs">Every pollSecs seconds, poll to see how long we've been away.</param>
         /// <param name="notifySecs">If we've been away notifySecs seconds, fire notification.</param>
-        public IdleTime(int pollSecs, int notifySecs)
+        public IdleTime(int pollSecs, int notifySecs) : this()
         {
-            m_pollSecs = pollSecs;
-            m_notifySecs = notifySecs;
-            if (m_pollSecs > m_notifySecs)
+            if (pollSecs > notifySecs)
                 throw new ArgumentException("Poll more often than you notify.");
-            m_timer = new System.Timers.Timer(m_pollSecs);
-            m_timer.Elapsed += new System.Timers.ElapsedEventHandler(m_timer_Elapsed);
+            PollInterval = pollSecs;
+            IdleLength = notifySecs;
         }
 
         /// <summary>
         /// Is the timer running?
         /// </summary>
+        [Category("Logic")]
+        [DefaultValue(false)]
         public bool Enabled
         {
             get { return m_timer.Enabled; }
@@ -94,20 +108,75 @@ namespace bedrock.util
         }
 
         /// <summary>
-        /// Fire events in the GUI thread for this control.
+        /// Time, in seconds, between checking for 
         /// </summary>
-        public System.ComponentModel.ISynchronizeInvoke InvokeControl
+        [Category("Time")]
+        [DefaultValue(DEFAULT_POLL)]
+        public double PollInterval
         {
-            get { return m_invoker;  }
+            get { return m_timer.Interval / 1000.0; }
+            set { m_timer.Interval = value * 1000.0; }
+        }
+
+        /// <summary>
+        /// The amount of time (in seconds) the computer can be idle before OnIdle is fired.
+        /// </summary>
+        [Category("Time")]
+        [DefaultValue(DEFAULT_IDLE)]
+        public double IdleLength
+        {
+            get { return m_notifySecs;  }
+            set { m_notifySecs = value;  }
+        }
+
+        /// <summary>
+        /// Are we currently idle?
+        /// </summary>
+        [Category("Logic")]
+        public bool IsIdle
+        {
+            get { return m_idle; }
+        }
+
+        /// <summary>
+        /// Invoke() all callbacks on this control.
+        /// </summary>
+        [Description("Invoke all callbacks on this control")]
+        [DefaultValue(null)]
+        [Category("Logic")]
+        public ISynchronizeInvoke InvokeControl
+        {
+            get
+            {
+                // If we are running in the designer, let's try to get
+                // an invoke control from the environment.  VB
+                // programmers can't seem to follow directions.
+                if ((this.m_invoker == null) && DesignMode)
+                {
+                    IDesignerHost host = (IDesignerHost)base.GetService(typeof(IDesignerHost));
+                    if (host != null)
+                    {
+                        object root = host.RootComponent;
+                        if ((root != null) && (root is ISynchronizeInvoke))
+                        {
+                            m_invoker = (ISynchronizeInvoke)root;
+                            // TODO: fire some sort of propertyChanged event,
+                            // so that old code gets cleaned up correctly.
+                        }
+                    }
+                }
+                return m_invoker;
+            }
             set { m_invoker = value; }
         }
+
 
         private void m_timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
             double idle = GetIdleTime();
             if (m_idle)
             {
-                if (idle < m_pollSecs)
+                if (idle < PollInterval)
                 {
                     m_idle = false;
                     if (OnUnIdle != null)
