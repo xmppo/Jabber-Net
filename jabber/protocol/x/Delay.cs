bf2063ae5a0f84257de294fa92f153c1afdc07f2
hildjj <hildjj@b87ccc53-a82b-0410-9262-3dc31cef668b>
2008-03-12 16:25:24 +0000

    Add support for XEP-0203.
    git-svn-id: http://jabber-net.googlecode.com/svn/trunk@636 b87ccc53-a82b-0410-9262-3dc31cef668b

--- jabber/protocol/x/Delay.cs
+++ jabber/protocol/x/Delay.cs
@@ -71,4 +71,63 @@ namespace jabber.protocol.x
             set { this.InnerText = value; }
         }
     }
+
+    /// <summary>
+    /// A modern, XEP-0203 delay element
+    /// </summary>
+    public class ModernDelay : Element
+    {
+/*
+ <delay xmlns='urn:xmpp:delay'
+     from='capulet.com'
+     stamp='2002-09-10T23:08:25Z'>
+    Offline Storage
+  </delay>
+*/
+        /// <summary>
+        ///Create a delay element for sending
+        /// </summary>
+        /// <param name="doc"></param>
+        public ModernDelay(XmlDocument doc) : base("delay", URI.DELAY, doc)
+        {
+        }
+
+        /// <summary>
+        /// Create a delay element from the received stream.
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="qname"></param>
+        /// <param name="doc"></param>
+        public ModernDelay(string prefix, XmlQualifiedName qname, XmlDocument doc) :
+            base(prefix, qname, doc)
+        {
+        }
+
+        /// <summary>
+        /// From whom?
+        /// </summary>
+        public string From
+        {
+            get { return GetAttribute("from"); }
+            set { SetAttribute("from", value); }
+        }
+
+        /// <summary>
+        /// Date/time stamp.
+        /// </summary>
+        public DateTime Stamp
+        {
+            get { return Element.DateTimeProfile(GetAttribute("stamp")); }
+            set { SetAttribute("stamp", Element.DateTimeProfile(value)); }
+        }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Desc
+        {
+            get { return this.InnerText; }
+            set { this.InnerText = value; }
+        } 
+    }
 }
