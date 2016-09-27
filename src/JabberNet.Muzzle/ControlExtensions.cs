d5f175e2cd704ae57430ddd0f5d4a3f5377f322c
Friedrich von Never <friedrich@fornever.me>
2016-09-27 22:57:41 +0700

    #46: add a class to invoke stuff on controls

---
+++ src/JabberNet.Muzzle/ControlExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace JabberNet.Muzzle
+{
+    /// <summary>
+    /// Extension methods for <see cref="Control"/>.
+    /// </summary>
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// Invoke the action in the control owning thread if invocation is required.
+        /// </summary>
+        /// <param name="control">Control on which the action should be invoked.</param>
+        /// <param name="action">The invocable action.</param>
+        public static void InvokeAction(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
