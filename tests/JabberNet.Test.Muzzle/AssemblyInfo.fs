758c3a5b38d99a311c24998336ee20d2a9c5d3aa
Friedrich von Never <friedrich@fornever.me>
2016-09-29 23:35:01 +0700

    #46: move Muzzle test to a separate assembly

---
+++ tests/JabberNet.Test.Muzzle/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace JabberNet.Test.Muzzle.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyCompany("Jabber-Net contributors")>]
+[<assembly: AssemblyProduct("Jabber-Net")>]
+[<assembly: AssemblyTitle("JabberNet.Test.Muzzle")>]
+[<assembly: AssemblyCopyright("Copyright (c) Jabber-Net Contributors, 2008—2016")>]
+[<assembly: AssemblyVersion("2.1.2.0")>]
+[<assembly: Guid("f53c28b5-f626-4083-9ce6-99c9a9fe860b")>]
+
+do ()
