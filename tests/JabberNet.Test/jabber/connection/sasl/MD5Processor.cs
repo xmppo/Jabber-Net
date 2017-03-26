70f674cb9495b0b94273013d697b93daf33d22de
Friedrich von Never <friedrich@fornever.me>
2017-03-26 14:33:09 +0700

    #86: add test

--- tests/JabberNet.Test/jabber/connection/sasl/MD5Processor.cs
+++ tests/JabberNet.Test/jabber/connection/sasl/MD5Processor.cs
@@ -1,4 +1,4 @@
-/* --------------------------------------------------------------------------
+﻿/* --------------------------------------------------------------------------
  * Copyrights
  *
  * Portions created by or assigned to Cursive Systems, Inc. are
@@ -12,6 +12,8 @@
  * See licenses/Jabber-Net_LGPLv3.txt for details.
  * --------------------------------------------------------------------------*/
 
+using System;
+using System.Text;
 using System.Xml;
 using JabberNet.jabber.connection.sasl;
 using JabberNet.jabber.protocol.stream;
@@ -25,7 +27,6 @@ namespace JabberNet.Test.jabber.connection.sasl
         [Test]
         public void TestChallenge()
         {
-
             XmlDocument doc = new XmlDocument();
             Challenge c = new Challenge(doc);
             c.InnerText = "cmVhbG09IndlYjIwMDMiLCBub25jZT0iWWE0anVNYzU0SG9UWDBPa1VPRDFvQT09IiwgcW9wPSJhdXRoLCBhdXRoLWludCIsIGNoYXJzZXQ9dXRmLTgsIGFsZ29yaXRobT1tZDUtc2Vzcw==";
@@ -37,6 +38,28 @@ namespace JabberNet.Test.jabber.connection.sasl
             Assert.IsNotNull(s);
             Assert.AreEqual("Ya4juMc54HoTX0OkUOD1oA==", m["nonce"]);
             Assert.AreEqual("auth, auth-int", m["qop"]);
+        }
+
+        [Test]
+        public void QopAuthShouldBeAssumedIfNoQopIsPresented()
+        {
+            var doc = new XmlDocument();
+            var challenge = "realm=\"cthulhu-3\",nonce=\"IhS5TwCvOOaJE7hdwQQYjHCU5n5tLONF4/0Jc43r\",charset=utf-8,algorithm=md5-sess";
+            var c = new Challenge(doc)
+            {
+                InnerText = Convert.ToBase64String(Encoding.UTF8.GetBytes(challenge))
+            };
+
+            var m = new MD5Processor
+            {
+                ["username"] = "user2",
+                ["password"] = "123"
+            };
+            var s = m.step(c, doc);
+
+            Assert.IsNotNull(s);
+            Assert.AreEqual("IhS5TwCvOOaJE7hdwQQYjHCU5n5tLONF4/0Jc43r", m["nonce"]);
+            Assert.Null(m["qop"]);
         }
     }
 }
